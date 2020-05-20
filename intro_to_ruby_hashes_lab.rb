845d0e644d2675c3a5d0aa8bb97a95c62d95beb0
Matine temory <temorymatine@gmail.com>
2020-05-20 21:14:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,10 @@
 def base_hash
-  monopoly={:railroads=>{}}
+  monopoly={:railroads => {}}
 
 end
 
 def monopoly_with_second_tier
-   monopoly={:railroads=>{}}
+   monopoly={:railroads => {}}
 end
 
 def monopoly_with_third_tier
