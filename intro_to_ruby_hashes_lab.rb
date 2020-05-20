afa276e978986db62b27b1eb52460e18ea07a759
Matine temory <temorymatine@gmail.com>
2020-05-20 21:19:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,15 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-   monopoly={:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25}, :names => {}}}
+   monopoly=
+   {:railroads => 
+        {:pieces => 4, :rent_in_dollars => 
+              {:one_piece_owned => 25}, 
+                :names => {}
+          
+        }
+     
+   }
 end
 
 end
